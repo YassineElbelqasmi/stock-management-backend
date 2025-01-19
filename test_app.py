9131e800118e95f92104fc6adac102319b732b06
YassineElbelqasmi <yassineservicelivraison@gmail.com>
2025-01-19 08:47:18 +0100

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,7 @@
+import pytest
+from app import app
+
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.json == {'message': 'Welcome to the Stock Management Backend'}
